14d4fcb6a979ac59bcbd55403e443813f1bdd4ad
James Higgins and Nick Powell <nick@>
2017-06-27 09:27:37 -0700

    add create and show for lessons

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,28 @@
+class LessonsController < ApplicationController
+  def index
+    @lessons = Lesson.all
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      redirect_to  lessons_path
+    else
+      render :new
+    end
+  end
+
+private
+  def lesson_params
+    params.require(:lesson).permit(:name, :content, :number)
+  end
+
+end
